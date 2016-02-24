b1c708991a2fcb7c86f40d374ebfce87b491b549
Nicolas Mercier <mercier@plaste.fr>
2016-02-24 15:21:27 +0100

    gitignore 3

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_NAME', 'ascometal');
 define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
@@ -78,6 +78,8 @@ $table_prefix  = 'wp_rZp54_';
  * développement.
  */
 define('WP_DEBUG', false);
+
+define('RELOCATE', true);
 
 /* C'est tout, ne touchez pas à ce qui suit ! Bon blogging ! */
 
