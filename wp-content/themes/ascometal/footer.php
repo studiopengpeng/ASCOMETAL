b94f264069486e94845845ee4ee9215e0dfc46c8
Nicolas Mercier <mercier@plaste.fr>
2016-03-10 17:19:32 +0100

    Accordéons tests 01

--- wp-content/themes/ascometal/footer.php
+++ wp-content/themes/ascometal/footer.php
@@ -57,7 +57,7 @@ wp_nav_menu( $args_menu1 );
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
 <!--live reload-->
 <script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.11.1.js'><\/script>".replace("HOST", location.hostname));
+    //document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.11.1.js'><\/script>".replace("HOST", location.hostname));
 //]]></script>
 <!--live reload END-->
 </body>
