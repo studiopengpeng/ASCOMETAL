b3cf70a30b80056571e59ed399910318bdeea839
Philippe Petitgenet <philippe@petitgenet.com>
2016-03-23 16:56:40 +0100

    tentative Réparation comit

--- wp-content/themes/ascometal/index.php
+++ wp-content/themes/ascometal/index.php
@@ -15,16 +15,22 @@
 
 get_header(); ?>
 
-<div id="page" role="main">
-<!--header de page : contient le bandeau image + le titre de la rubrique principale-->
-        <article class="small-12 medium-12 large-12 columns">
+    <div id="page" role="main">
+        <!--header de page : contient le bandeau image + le titre de la rubrique principale-->
+        <div class="row">
+            <article class="small-12 medium-12 large-12 columns">
                 <header class="header-image">
                     <?php get_template_part( 'template-parts/featured-image' ); ?>
-                    <h1>Actualités</h1>
+                        <h1>Actualités</h1>
                 </header>
-        </article>
+            </article>
+        </div>
+       
         <!--END header de page-->
-        <?php do_action( 'foundationpress_before_content' ); ?>
+
+
+ <div class="row">
+   <?php do_action( 'foundationpress_before_content' ); ?>
                     <!--menu secondaire : Menu gauche -> page.php-->
                     <?php
 $args_menu1 = array(
@@ -48,37 +54,35 @@ $args_menu1 = array(
 wp_nav_menu( $args_menu1 );
 ?>
                         <!--END menu secondaire-->
-	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
+                      
+<!-- J'ai retiré la classe main-content qui était appliquée sur le div article et ça roule, elle doit comprendre des css qui entrent en conflit. Peut être que le main content à des propriétés spéciales... Le main-content ne doit peut être pas s'appliquer sur ce genre de listes mais juste sur le contenu d'un article. -->
+        <article class="small-12 medium-9 large-9 columns"> 
+                <?php if ( have_posts() ) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
+                    <?php /* Start the Loop */ ?>
+                        <?php while ( have_posts() ) : the_post(); ?>
+                            <?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+                        <?php endwhile; ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+                <?php else : ?>
+                    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+                <?php endif; // End have_posts() check. ?>
 
-		<?php endif; // End have_posts() check. ?>
+                <?php /* Display navigation to next/previous pages when applicable */ ?>
+                    <?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+                        <nav id="post-nav">
+                            <div class="post-previous">
+                                <?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?>
+                            </div>
+                            <div class="post-next">
+                                <?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?>
+                            </div>
+                        </nav>
+                    <?php } ?>
 
-		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php } ?>
+            </article>
+                      
+                       </div>
+                    </div>
 
-	</article>
-	
-	<?php get_sidebar(); ?>
-
-</div>
-
-<?php get_footer();
-
-
-
-
-
-
+    <?php get_footer();
--- wp-content/themes/ascometal/template-parts/content.php
+++ wp-content/themes/ascometal/template-parts/content.php
@@ -8,27 +8,36 @@
  * @since FoundationPress 1.0.0
  */
 
+// If a feature image is set, get the id, so it can be injected as a css background property
+	if ( has_post_thumbnail( $post->ID ) ) :
+		$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'vignette_actu' );
+		$image = $image[0];
 ?>
-    <div id="post-<?php the_ID(); ?>" <?php post_class( 'blogpost-entry'); ?>>
-        <article class="ih-item">
-            <header>
-<!--                <p>Lien vers fichier parmettant l'affichage des derniers articles dans archive.php = <strong>template-parts/content.php</strong></p>-->
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <?php the_post_thumbnail('vignette_actu'); ?>
-
-                    <?php foundationpress_entry_meta(); ?>
-            </header>
-
-            <div class="entry-content">
-                <?php the_content( __( 'Voir +', 'foundationpress' ) ); ?>
+    <div class="ih-item article-preview" id="post-<?php the_ID(); ?>" <?php post_class( 'blogpost-entry'); ?>>
+            <!--                <p>boucle vers fichier parmettant l'affichage des derniers articles dans archive.php = <strong>template-parts/content.php</strong></p>-->
+            <div class= "thumb small-12 medium-3 large-3">
+                <a ref="<?php the_permalink(); ?>">
+                    <div style="background-image: url('<?php echo $image ?>')">
+                    </div>
+                </a>
             </div>
-            <footer>
-                <?php $tag = get_the_tags(); if ( $tag ) { ?>
-                    <p>
-                        <?php the_tags(); ?>
-                    </p>
-                    <?php } ?>
-            </footer>
-            </article>
-            <hr />
+            <?php endif; ?>
+            <div  class="infos small-12 medium-9 large-9">
+            <?php foundationpress_entry_meta(); ?>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+        <div class="entry-content">
+            <?php the_content( __( 'Voir +', 'foundationpress' ) ); ?>
+        </div>
+        </div>
+        <footer>
+            <?php $tag = get_the_tags(); if ( $tag ) { ?>
+                <p>
+                    <?php the_tags(); ?>
+                </p>
+                <?php } ?>
+        </footer>
     </div>
+    
+   
+  
