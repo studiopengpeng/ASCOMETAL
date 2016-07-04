5bdab3105ca359f9f7bfd3729038ae3855394786
Nicolas Mercier <mercier@plaste.fr>
2016-07-04 15:32:40 +0200

    Corrections calculatrices
    js non lié...

--- wp-content/themes/ascometal/page-templates/page-calculatrice-carbone.php
+++ wp-content/themes/ascometal/page-templates/page-calculatrice-carbone.php
@@ -124,5 +124,5 @@ wp_nav_menu( $args_menu1 );
 
 
     </div>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/javascript/custom/convertisseur.js">
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/javascript/custom/carbonne.js">
     <?php get_footer();
--- wp-content/themes/ascometal/page-templates/page-calculatrice-masse.php
+++ wp-content/themes/ascometal/page-templates/page-calculatrice-masse.php
@@ -145,5 +145,5 @@ wp_nav_menu( $args_menu1 );
 
 
     </div>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/javascript/custom/convertisseur.js">
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/javascript/custom/calculmasse.js">
     <?php get_footer();
